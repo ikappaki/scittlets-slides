5899f3eac8573fb35b71d31a91836ebffd66d284
ikappaki <ikappaki@users.noreply.github.com>
2025-05-12 18:41:42 +0100

    update mermaid deps to v0.1.0

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Presentation: https://ikappaki.github.io/scittlets-slides/
 
-🍴 Made from [Chris McCormick](https://github.com/chr15m)'s [Scittle Tiny Slides](https://github.com/chr15m/scittle-tiny-slides).
+🍴 Made of [Chris McCormick](https://github.com/chr15m)'s [Scittle Tiny Slides](https://github.com/chr15m/scittle-tiny-slides).
--- slides.cljs
+++ slides.cljs
@@ -65,19 +65,20 @@ journey
    [:section
     [slides-counter-header state]
     [:h1 "Links"]
-    [:ul {:style {:list-style "none"
-                  :margin "0 auto"
-                  :padding "0"
-                  :text-align "center"}}
-     [:li [:span
-           "Slides: "
-           [:a {:href "https://github.com/ikappaki/scittlets-slides"} "https://github.com/ikappaki/scittlets-slides"]]]
-     [:li [:span
-           "Scittlets: "
-           [:a {:href "https://github.com/ikappaki/scittlets"} "https://github.com/ikappaki/scittlets"]]]
-     [:li [:span
-           "Scittle: "
-           [:a {:href "https://github.com/babashka/scittle"} "https://github.com/babashka/scittle"]]]]]
+
+    [:div {:style {:display "flex"
+                   :justify-content "center"
+                   :align-items "center"}}
+     [:ul
+      [:li [:span
+            "Slides: "
+            [:a {:href "https://github.com/ikappaki/scittlets-slides"} "https://github.com/ikappaki/scittlets-slides"]]]
+      [:li [:span
+            "Scittlets: "
+            [:a {:href "https://github.com/ikappaki/scittlets"} "https://github.com/ikappaki/scittlets"]]]
+      [:li [:span
+            "Scittle: "
+            [:a {:href "https://github.com/babashka/scittle"} "https://github.com/babashka/scittle"]]]]]]
    
    ])
 
